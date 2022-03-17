79589fd75a6ce9022e20cc870f317742d2392d48
jdavis19unc <89363700+jdavis19unc@users.noreply.github.com>
2022-03-17 01:25:35 -0400

    Adding Gif

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -1,5 +1,5 @@
 
-import basicListIteration from "../images/BasicListIteration.PNG"
+import basicListIteration from "../images/BasicListIteration.gif"
 
 let im = <img src = {basicListIteration} height="300px"></img>;
 
@@ -10,10 +10,14 @@ function Main(){
         <h1>My Visualizations</h1>
         <ul>
             <li>
-        <a href="http://127.0.0.1:5501/my-app/src/visualizations/IterateList/index.html" target="_blank">-Basic List Iteration</a>
-        </li>
+        <a href="http://127.0.0.1:5501/my-app/src/visualizations/IterateList/index.html" target="_blank">
+        <h1>Basic List Iteration</h1>
         {im}
+        </a>
+        </li>
+        
         </ul>
+        <footer>@Jackson Davis 2022</footer>
         </div>
     )
 }
